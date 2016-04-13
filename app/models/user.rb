391ef71bcf39b9934a7a2c923c0334701e8b2a0a
Katsunori Kawaguchi <hamakata@gmail.com>
2016-04-13 19:02:40 +0900

    Fix user model and add sample specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :name, length: { minimum: 2 }
+  validates :email, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:name) { "" }
+  let(:email) { "foo@test.test" }
+  let(:user_params) { { name: name, email: email } }
+  let(:user) { User.new user_params }
+
+  describe "#name" do
+    context "name.length < 2" do
+      let(:name) { "a" }
+      it "should be invalid" do
+        user.valid?
+        expect(user.errors[:name]).to be_present
+      end
+    end
+
+    context "name.length >= 2" do
+      let(:name) { "aa" }
+      it "should be invalid" do
+        user.valid?
+        expect(user.errors[:name]).to be_empty
+      end
+    end
+  end
 end
